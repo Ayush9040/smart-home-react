cd7bd22d4433f67e43d8bd2afae50fdc713d9fd3
Ayush9040 <68855277+Ayush9040@users.noreply.github.com>
2023-02-18 19:41:14 +0530

    portfolio project

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,24 +7,24 @@ const Header = () => {
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div class="collapse navbar-collapse sh-nav-wrapper" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="#">Home</a>
+                        <a class="nav-link sh-nav-left_links" aria-current="page" href="#">Home</a>
                         </li>
                         <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
+                        <a class="nav-link sh-nav-left_links" href="#">About</a>
                         </li>
                         <li class="nav-item">
-                        <a class="nav-link" href="#">Testimonials</a>
+                        <a class="nav-link sh-nav-left_links" href="#">Testimonials</a>
                         </li>
                         <li class="nav-item">
-                        <a class="nav-link" href="#">Contact</a>
+                        <a class="nav-link sh-nav-left_links" href="#">Contact</a>
                         </li>
                     </ul>
                     <form class="d-flex align-items-center">
-                        <li><a href='/'>Login</a></li>
-                        <button class="btn btn-outline-success" type="submit">Sign Up</button>
+                        <li><a className='sh-nav-right_links' href='/'>Login</a></li>
+                        <button class="btn btn-outline-success sh-nav-right_btn" type="submit">Sign Up</button>
                     </form>
                     </div>
             </div>
