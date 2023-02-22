eb78bbe80f8107616ef8a6f5cd86c2a9e25ec907
Ayush9040 <68855277+Ayush9040@users.noreply.github.com>
2023-02-22 15:42:10 +0530

    client section done

--- src/components/Home/Clients.js
+++ src/components/Home/Clients.js
@@ -3,10 +3,10 @@ import React from 'react'
 
 const clientsImg = [
     {
-        img: '/images/Home/lifefitness.png',
+        img: '/images/Home/travel.png',
     },
     {
-        img: '/images/Home/lifefitness.png',
+        img: '/images/Home/octane.png',
     },
     {
         img: '/images/Home/lifefitness.png',
@@ -15,16 +15,16 @@ const clientsImg = [
         img: '/images/Home/smile.png',
     },
     {
-        img: '/images/Home/lifefitness.png',
+        img: '/images/Home/true.png',
     },
     {
-        img: '/images/Home/lifefitness.png',
+        img: '/images/Home/cybex.png',
     },
     {
-        img: '/images/Home/lifefitness.png',
+        img: '/images/Home/mockup.png',
     },
     {
-        img: '/images/Home/lifefitness.png',
+        img: '/images/Home/precor.png',
     }
   ];
 
@@ -37,14 +37,23 @@ const Clients = () => {
                     <h2 className='sh-client__heading'>Our Clients</h2>
                     <p className='sh-client__content'>Wisdom new and valley answer. Contented it so is discourse recommend. Man its upon him call mile. An pasture he himself believe ferrars besides cottage.</p>
                     <div className='sh-client__main-img-container d-flex flex-column pt-4'>
-                        <div className='sh-client__img-row d-flex align-items-center justify-content-center g-4'>
+                        <div className='row g-4'>
 
              {
                             clientsImg.map((data, index) => {
                                 return (
-                                    <div className='col-12 sh-client__img-wrapper' key={index}>
+                                    // <div className='col-12 sh-client__img-wrapper' key={index}>
+                                    //     <img className='sh-client__img' src={data.img} />
+                                    // </div>
+                                  <div className='col-6 col-lg-3 ' key={index}>
+                                      <div  className='sh-client__logos-outwrap w-100 d-flex align-items-center justify-content-center'>
+                                         <div className='sh-client__logos-inwrap d-flex align-items-center justify-content-center' >
                                         <img className='sh-client__img' src={data.img} />
                                     </div>
+                                    </div>
+                                  </div>
+                                    
+
                                 )
                             })
             }
